79b0f1bc02ff13d5e739b11494ec0a13cab739cf
HezMakLak <hezekiah.mekonen@yahoo.com>
2024-12-20 15:04:41 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './PAGES/Home/Home.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Heloo</h1>
+      <Home/>    
     </div>
   );
 }
